715e828af9f187a75fbabbab297d841ddc477c3e
yaldaj19 <jyalda.619@gmail.com>
2021-01-17 11:02:57 +0330

    added new files in repository

---
+++ script.js
@@ -0,0 +1,26 @@
+function LoopAFunc(){
+    for(var i=1;i<10;i++){
+        console.log(i+ 'A');
+    }
+}
+
+function LoopBFunc(){
+    for(var j=1;j<10;j++){
+        console.log(j+'B');
+    }
+}
+
+async function RunEventLoop(){
+   
+    let myPA=new Promise((LoopA)=>LoopAFunc())
+    let myPB=new Promise((LoopB)=>LoopBFunc())
+
+    await Promise.all;
+}
+
+RunEventLoop();
+
+
+
+
+
